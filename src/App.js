9d8d367d556afee746d351ed783170dd99d96780
Kumar Sambhav <krsambhav16@gmail.com>
2022-10-06 21:50:17 +0530

    Added Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import {IoIosArrowBack} from 'react-icons/io';
+import {BiBell} from 'react-icons/bi';
 
 function App() {
   return (
     <div className="main w-screen h-screen flex flex-row justify-center items-center">
-      <div className="app-container shadow-md w-[350px] h-[600px]"></div>
+      <div className="app-container shadow-md w-[330px] h-[600px] flex flex-col py-5">
+        <div className="nav-bar flex flex-row justify-between px-5">
+          <IoIosArrowBack />
+          <BiBell />
+        </div>
+      </div>
     </div>
   );
 }
