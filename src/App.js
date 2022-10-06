ba13ab02dc3008832788451d71d037c7d7c21825
Kumar Sambhav <krsambhav16@gmail.com>
2022-10-06 21:55:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import {BiBell} from 'react-icons/bi';
 function App() {
   return (
     <div className="main w-screen h-screen flex flex-row justify-center items-center">
-      <div className="app-container shadow-md w-[330px] h-[600px] flex flex-col py-5">
-        <div className="nav-bar flex flex-row justify-between px-5">
+      <div className="app-container shadow-md w-[330px] h-[600px] flex flex-col py-5 items-center gap-5">
+        <div className="nav-bar flex flex-row justify-between w-[90%]">
           <IoIosArrowBack />
           <BiBell />
+        </div>
+        <div className="user-badge-container w-[90%] h-[80px] rounded-lg shadow">
+ 
         </div>
       </div>
     </div>
