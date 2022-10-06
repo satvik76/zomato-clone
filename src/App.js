b86f287cb97a9c84bb428ada1ee7704cb7a95cca
akshat3333 <57315902+akshat3333@users.noreply.github.com>
2022-10-06 21:41:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="main w-screen h-screen flex flex-row justify-center items-center">
+      <div className="app-container shadow-md w-[350px] h-[600px]"></div>
+    </div>
+  );
+}
+
+export default App;
